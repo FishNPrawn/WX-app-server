28cdf8b049814fb0c70240813edbbf1d1ba0a814
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-08-03 15:50:53 +0800

    publish

--- src/main/java/com/example/fishnprawn/global/GlobalConst.java
+++ src/main/java/com/example/fishnprawn/global/GlobalConst.java
@@ -1,7 +1,8 @@
 package com.example.fishnprawn.global;
 
 public class GlobalConst {
-    public static String BASE_DOMAIN = "http://localhost:8080";
+    // public static String BASE_DOMAIN = "http://localhost:8080";
     // public static String BASE_DOMAIN = "https://fishnprawn.cn"; // For 腾讯云
+    public static String BASE_DOMAIN = "https://joynfish.com";
 
 }
--- src/main/resources/static/js/domain.js
+++ src/main/resources/static/js/domain.js
@@ -1,2 +1,3 @@
-const BASE_DOMAIN = "http://localhost:8080"
+// const BASE_DOMAIN = "http://localhost:8080"
 // const BASE_DOMAIN = "https://fishnprawn.cn"     // For 腾讯云
+const BASE_DOMAIN = "https://joynfish.com"     // 发布
