882637c89e0b617566ef05059518f93400b998f4
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-08-24 11:04:27 +0800

    Create: create pay supplier

---
+++ src/main/java/com/example/fishnprawn/paySupplier/PaySupplierDao.java
@@ -0,0 +1,6 @@
+package com.example.fishnprawn.paySupplier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaySupplierDao extends JpaRepository<PaySupplier,Integer> {
+}
